5f35fa47aca4e825ef9b029331054e0603ae71a5
Joshua Needleman <joshuaneedleman@Joshuas-MBP.lan>
2024-10-13 11:35:22 -0400

    Created Hello component with name and age

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,15 +1,21 @@
-const App = () => {
-  const now = new Date()
-  const a = 10
-  const b = 20
-  console.log(now, a+b)
-
+const Hello = (props) => {
+  console.log(props)
   return (
     <div>
-      <p>Hello world, it is {now.toString()}</p>
-      <p>
-        {a} plus {b} is {a+b}
-      </p>
+      <p>Hello {props.name}, you are {props.age}</p>
+    </div>
+  )
+}
+
+
+const App = () => {
+  const name = 'Peter'
+  const age = 10
+  return (
+    <div>
+      <h1>Greetings</h1>
+    <Hello name='Martin' age ={26+10} />
+    <Hello name={name} age ={age} />
     </div>
   )
 }
