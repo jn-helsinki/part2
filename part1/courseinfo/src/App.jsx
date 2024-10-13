f5e1468fef6ed3757f422d3578a2fd930a4ae7f3
Joshua Needleman <joshuaneedleman@Joshuas-MBP.lan>
2024-10-13 11:50:46 -0400

    removed assets directory

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,7 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
