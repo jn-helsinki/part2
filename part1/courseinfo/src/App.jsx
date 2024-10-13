b2ef3babbcd5d278621539f440ad2194abbd64f7
Joshua Needleman <joshuaneedleman@Joshuas-MBP.lan>
2024-10-13 11:48:33 -0400

    Created new react app Course Info. Began outlining courses and num exercises.

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <>
+    <h1>{course}</h1>
+    <p>{part1} {exercises1}</p>
+    <p>{part2} {exercises2}</p>
+    <p>{part3} {exercises3}</p>
+    <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+    </>
+  )
+}
+
+export default App
