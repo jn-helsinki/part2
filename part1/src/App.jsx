310cd08bcd3e2313e3a5701d4bab9e73cc036982
Joshua Needleman <joshuaneedleman@Joshuas-MBP.lan>
2024-10-18 12:41:51 -0400

    made Display and Button components. added plus, minus, and zero buttons.

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -4,15 +4,30 @@ const App = (props) => {
 
   const [counter, setCounter] = useState(0)
 
+  const increment = () => setCounter(counter+1)
+  const setToZero = () => setCounter(0)
+  const decrement = () => setCounter(counter-1)
+
+  const Display = (props) => {
+    return (<>{props.counter}</>)
+  }
+
+  const Button = (props) => {
+    return (
+      <button onClick={props.onClick}>
+        {props.text}
+      </button>
+    )
+  }
 
 return (
   <>
-    <div>{counter}</div>
-    <button onClick={()=>setCounter(counter+1)}>
-      plus
-    </button>
-    <button onClick={()=>setCounter(0)}>
-      zero
-    </button>
+    <Display counter={counter} />
+    <div>
+    <Button onClick={increment} text='plus' />
+    <Button onClick={setToZero} text ='zero' />
+    <Button onClick = {decrement} text = 'minus' /></div>
+
+
   </>)}
 export default App
