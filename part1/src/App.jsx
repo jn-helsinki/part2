5f4d40b88269199998651954740fe513bf57d0d9
Josh Needleman <joshua.needleman510@gmail.com>
2024-10-18 17:26:48 -0400

    replaced with simple counter/reset button

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,44 +1,17 @@
 import { useState } from "react";
 
-const History = (props) => {
-  if (props.allClicks.length === 0) {
-    return <div>the app is used by pressing buttons</div>;
-  }
-  return <div>button press history: {props.allClicks.join(" ")}</div>;
-};
+const App = () => {
+  const [value, setValue] = useState(10);
 
-const Button = ({ handleClick, text }) => (
-  <button onClick={handleClick}>{text}</button>
-);
-
-const App = (props) => {
-  const [left, setLeft] = useState(0);
-  const [right, setRight] = useState(0);
-  const [total, setTotal] = useState(0);
-  const [allClicks, setAll] = useState([]);
-
-  const handleLeftClick = () => {
-    setAll(allClicks.concat("L"));
-    const updatedLeft = left + 1;
-    setLeft(updatedLeft);
-    setTotal(updatedLeft + right);
-  };
-
-  const handleRightClick = () => {
-    setAll(allClicks.concat("R"));
-    const updatedRight = right + 1;
-
-    setRight(updatedRight);
-    setTotal(left + updatedRight);
+  const handleClick = () => {
+    console.log("clicked the button");
+    setValue(0);
   };
 
   return (
     <div>
-      {left}
-      <Button handleClick={handleLeftClick} text={"left"} />
-      <Button handleClick={handleRightClick} text={"right"} />
-      {right}
-      <History allClicks={allClicks} />
+      {value}
+      <button onClick={handleClick}>reset to zero</button>
     </div>
   );
 };
