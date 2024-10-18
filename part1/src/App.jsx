b2a9f2a28ab45b9ef78442a08e33b8a4f217a599
Joshua Needleman <joshuaneedleman@Joshuas-MBP.lan>
2024-10-18 12:20:10 -0400

    added a counter that re-renders manually

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,31 +1,9 @@
-const Hello = ({name, age}) => {
-  // const {name, age} = props
 
-  // const name = props.name
-  // const age = props.age
-  const bornYear = () => new Date().getFullYear()-age
-
-  // console.log(props)
-  return (
-    <div>
-      <p>Hello {name}, you are {age} years old</p>
-      <p>So you were probably born in {bornYear()}</p>
-    </div>
-  )
-}
-
-
-const App = () => {
-  const name = 'Peter'
-  const age = 10
-  return (
-    <div>
-      <h1>Greetings</h1>
-    <Hello name='Martin' age ={26+10} />
-    <Hello name={name} age ={age} />
-    
-    </div>
-  )
+const App = (props) => {
+const {counter} = props
+return (
+  <div>{counter}</div>
+)
 }
 
 export default App
