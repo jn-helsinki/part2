49f9a369cc118e479636d2c6b3cd811a42feecd0
Josh Needleman <joshua.needleman510@gmail.com>
2024-10-18 17:08:13 -0400

    added LR array and explore asynchronous nature of state

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,33 +1,35 @@
 import { useState } from "react";
 
 const App = (props) => {
-  const [clicks, setClicks] = useState({
-    left: 0,
-    right: 0,
-  });
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
+  const [total, setTotal] = useState(0);
+  const [allClicks, setAll] = useState([]);
 
   const handleLeftClick = () => {
-    const newClicks = {
-      ...clicks,
-      left: clicks.left + 1,
-    };
-    setClicks(newClicks);
+    setAll(allClicks.concat("L"));
+    const updatedLeft = left + 1;
+    setLeft(updatedLeft);
+    setTotal(updatedLeft + right);
   };
 
   const handleRightClick = () => {
-    const newClicks = {
-      ...clicks,
-      right: clicks.right + 1,
-    };
-    setClicks(newClicks);
+    setAll(allClicks.concat("R"));
+    const updatedRight = right + 1;
+
+    setRight(updatedRight);
+    setTotal(left + updatedRight);
   };
+
   return (
     <div>
-      {clicks.left}
+      {left}
       <button onClick={handleLeftClick}>left</button>
 
       <button onClick={handleRightClick}>right</button>
-      {clicks.right}
+      {right}
+      <p>{allClicks.join(" ")}</p>
+      <p>total {total}</p>
     </div>
   );
 };
