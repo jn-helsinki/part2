acd883ff967a62fd09a57217a926ebbab3bbab46
Joshua Needleman <joshuaneedleman@Joshuas-MBP.lan>
2024-10-18 12:27:03 -0400

    made a counter component that increments on a set interval

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,9 +1,9 @@
+import {useState} from 'react'
 
 const App = (props) => {
-const {counter} = props
-return (
-  <div>{counter}</div>
-)
-}
+const [counter, setCounter] = useState(0)
+setTimeout(()=>setCounter(counter+1),1000)
+console.log('rendering...', counter)
 
+return (<div>{counter}</div>)}
 export default App
--- part1/src/main.jsx
+++ part1/src/main.jsx
@@ -2,10 +2,5 @@ import ReactDOM from 'react-dom/client'
 
 import App from './App'
 
-let counter = 1
-const refresh = () => ReactDOM.createRoot(document.getElementById('root')).render(<App counter = {counter}/>)
+ReactDOM.createRoot(document.getElementById('root')).render(<App/>)
 
-setInterval(()=> {
-    refresh()
-    counter+=1
-}, 1000)
