59ddf92899538e23894c4b56eb4792004b197995
Joshua Needleman <joshuaneedleman@Joshuas-MBP.lan>
2025-01-24 19:47:12 -0500

    Added phone numbers. Added name filter.

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,17 +1,24 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([{name:'Arto Hellas'}])
+  const [persons, setPersons] = useState([    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }])
 
 
 const [newName, setNewName] = useState('')
+const [newNumber, setNewNumber] = useState('')
+const [filter,setFilter] = useState('')
 
-const addName = (event) => {
+const addPerson = (event) => {
   event.preventDefault()
   console.log('button clicked', event.target)
 
   if (persons.every(person=> person.name.toLowerCase() !== newName.toLowerCase())){
-    setPersons(persons.concat({name:newName}))
+    setPersons(persons.concat({name:newName,number:newNumber, id:persons.length+1}))
+    setNewName('')
+    setNewNumber('')
   }
 
   else {
@@ -19,7 +26,7 @@ const addName = (event) => {
   }
 
 
-  setNewName('')
+
 }
 
 const handleNameChange = (event) => {
@@ -28,10 +35,22 @@ const handleNameChange = (event) => {
   setNewName(event.target.value)
 }
 
+const handleNumberChange = (event) =>{
+  setNewNumber(event.target.value)
+}
+
+
+const handleFilterChange = (event) => {
+  setFilter(event.target.value)
+}
+
+
 return (
   <div>
     <h2>Phonebook</h2>
-    <form onSubmit={addName} >
+    <div>filter:<input value={filter} onChange={handleFilterChange} /> </div>
+    <h2>Add a new</h2>
+    <form onSubmit={addPerson} >
       <div>
         name: 
         <input
@@ -40,11 +59,17 @@ return (
         />
       </div>
       <div>
+        number: 
+        <input 
+        value={newNumber}
+        onChange={handleNumberChange} />
+      </div>
+      <div>
         <button type="submit">add</button>
       </div>
     </form>
     <h2>Numbers</h2>
-    <div><ul>{persons.map(person => <li key={person.name}>{person.name}</li>)}</ul></div>
+    <div><ul>{persons.filter(person=>person.name.toLowerCase().startsWith(filter.toLowerCase())).map(person => <li key={person.name}>{person.name} {person.number}</li>)}</ul></div>
   </div>
 )
 }
