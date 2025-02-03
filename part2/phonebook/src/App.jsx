5c5a1fbb3fcbb6fd2e79d59fc218c6aec9941bcc
Joshua Needleman <joshuaneedleman@Joshuas-MacBook-Pro.local>
2025-02-02 20:51:16 -0500

    made some separate components

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,5 +1,20 @@
 import { useState } from 'react'
 
+const RenderPerson = (props) => {
+  return (<li key={props.name}>{props.name} {props.number}</li>)
+}
+
+const List = (props) => {
+  return (<><ul>{props.persons.filter(person=>person.name.toLowerCase().startsWith(props.filter.toLowerCase())).map(person => <RenderPerson key={person.name} name={person.name} number={person.number} />)}</ul></>)
+  // return <><ul>{props.persons.filter(person=>person.name.toLowerCase().startsWith(filter.toLowerCase())).map(person => <li key={person.name}>{person.name} {person.number}</li>)}</ul></>
+
+}
+
+const Filter = (props) =>{
+
+  return (<div>{props.text}:<input value={props.value} onChange={props.onChange} /> </div>)
+}
+
 const App = () => {
   const [persons, setPersons] = useState([    { name: 'Arto Hellas', number: '040-123456', id: 1 },
     { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
@@ -48,7 +63,8 @@ const handleFilterChange = (event) => {
 return (
   <div>
     <h2>Phonebook</h2>
-    <div>filter:<input value={filter} onChange={handleFilterChange} /> </div>
+    {/* <div>filter:<input value={filter} onChange={handleFilterChange} /> </div> */}
+    <Filter text={"filter"} value={filter} onChange={handleFilterChange} />
     <h2>Add a new</h2>
     <form onSubmit={addPerson} >
       <div>
@@ -69,7 +85,13 @@ return (
       </div>
     </form>
     <h2>Numbers</h2>
-    <div><ul>{persons.filter(person=>person.name.toLowerCase().startsWith(filter.toLowerCase())).map(person => <li key={person.name}>{person.name} {person.number}</li>)}</ul></div>
+ 
+ 
+    <div>
+      {/* <ul>{persons.filter(person=>person.name.toLowerCase().startsWith(filter.toLowerCase())).map(person => <li key={person.name}>{person.name} {person.number}</li>)}</ul> */}
+      <List persons={persons} filter={filter} />
+      
+      </div>
   </div>
 )
 }
